46c0c37815971125512685bb0cf3cc76a567e4d3
sumit kushwah <sk17595221@gmail.com>
2022-05-01 00:04:23 +0530

    Create DAY1.js
    this is my work on first day of javascript

---
+++ DAY1.js
@@ -0,0 +1,32 @@
+let x=5;
+let y = 10;
+console.log(x+y);
+/*
+Declare four variables without assigning values
+*/
+let t ;
+let su ;
+let k ;
+let yyy ;
+let Y ;
+/*
+Declare variables to store your first name, last name, marital status, country and age in multiple lines
+*/
+let firstname = "sumit";
+let lastname = "Kushwah";
+let maritalstatus = "unmarried";
+let country = "India";
+let age = 24;
+console.log(firstname,lastname,maritalstatus,country,age);
+/*
+Declare variables to store your first name, last name, marital status, country and age in a single line
+
+*/
+var Firstname="sumit",Lastname="Kushwah",Mritalstatus="yes",Country="india",Age="13";
+/*
+Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+*/
+var Myage = "I am 25 years old.";
+var yourAge ="You are 30 years old.";
+console.log(Myage);
+console.log(yourAge);
