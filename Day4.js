7561cd36869b9ea1f36523a4381cd7b3df123b8d
sumit kushwah <sk17595221@gmail.com>
2022-05-05 23:36:34 +0530

    Create Day4.js
    this is a Day4 coding

---
+++ Day4.js
@@ -0,0 +1,33 @@
+//How to create an empty array
+const arr = Array()
+console.log(arr)
+//Using square brackets([])
+const ar = []
+console.log(ar)
+// creating array with value
+const numbers = [0,3.14,9.81,37,98.6,100]
+const fruits = ['banana','Apple','Mango','papaya','orange']
+const vegetables = ['onion','pateto','tometo','Carrot','Cabbage']
+const webtech =['Html','css','js','pytrhon','Java']
+const country =['India','Newzeland','America','China','Russia']
+console.log('Number:',numbers)
+console.log(numbers.length)
+console.log('Fruits:',fruits)
+console.log(fruits.length)
+console.log('Vegetables:',vegetables)
+console.log('Webtech',webtech)
+console.log(webtech.length)
+console.log('Country:',country)
+console.log(country.length)
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+] // arr containing different data types
+console.log(arr)
+//Creating an array using split
+let js = 'javascript'
+const google = js.split('')
+console.log(google)
